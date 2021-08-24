3980b21b1e724c6366734dec40b7999717106419
Sarwan Dahri <iamsarwandahri@gmail.com>
2021-08-24 22:32:41 +0500

    Master

--- app/src/main/java/com/example/v2a/MainActivity.java
+++ app/src/main/java/com/example/v2a/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.v2a;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.media.MediaPlayer;
@@ -9,6 +10,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -21,20 +23,23 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.karumi.dexter.Dexter;
-import com.karumi.dexter.MultiplePermissionsReport;
 import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
-import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private Button pick, play, convert;
     private MediaPlayer mediaPlayer;
     private SurfaceView surfaceView;
     private String sourcePath, destPath, destDirectory, fileName;
+    private  TextView textView;
+    PermissionDeniedResponse permissonDeniedResponse = null;
+    PermissionGrantedResponse permissionGrantedResponse;
 
 
     @SuppressLint("SetTextI18n")
@@ -42,78 +47,124 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        surfaceView = findViewById(R.id.surfaceView);
+        pick = findViewById(R.id.pick);
+        play = findViewById(R.id.play);
+        convert = findViewById(R.id.convert);
+        textView = findViewById(R.id.textView);
+        play.setVisibility(View.INVISIBLE);
+        convert.setVisibility(View.INVISIBLE);
+
+
+
         Dexter.withContext(this)
-                .withPermissions(
-                        Manifest.permission.READ_EXTERNAL_STORAGE,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE
-                ).withListener(new MultiplePermissionsListener() {
-            @Override
-            public void onPermissionsChecked(MultiplePermissionsReport report) {
-                sourcePath = "/storage/emulated/0/music.mp4";
-                destDirectory = "/storage/emulated/0/V2A/";
-                fileName = "Myaudio.mp3";
-                destPath = destDirectory+fileName;
+                .withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        sourcePath = "/storage/emulated/0/music.mp4";
+                        destDirectory = "/storage/emulated/0/V2A/";
+                        fileName = "Myaudio.mp3";
+                        destPath = destDirectory+fileName;
 
-                File mediaStorageDir = new File(Environment.getExternalStorageDirectory(), "V2A");
-                if (!mediaStorageDir.exists()) {
-                    if (!mediaStorageDir.mkdirs()) {
-                        Log.d("App", "failed to create directory");
-                    }
-                }
+                        File mediaStorageDir = new File(Environment.getExternalStorageDirectory(), "V2A");
+                        if (!mediaStorageDir.exists()) {
+                            if (!mediaStorageDir.mkdirs()) {
+                                Log.d("App", "failed to create directory");
+                            }
+                        }
 
-                pick = findViewById(R.id.pick);
-                play = findViewById(R.id.play);
-                convert = findViewById(R.id.convert);
-                surfaceView = findViewById(R.id.surfaceView);
+                        convert.setOnClickListener(v -> {
+                            AudioExtractor audioExtractor = new AudioExtractor();
+                            try {
+                                audioExtractor.VideotoAudio(sourcePath, destPath, true, false);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
 
-                play.setVisibility(View.INVISIBLE);
-                convert.setVisibility(View.INVISIBLE);
+                            AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                            alert.setCancelable(true);
+                            alert.setCancelable(false);
+                            alert.setTitle("Convert Finished");
+                            alert.setMessage("Your file is saved in: " + destPath);
+                            alert.setPositiveButton("close", (dialog, which) -> {
+                                dialog.dismiss();
+                            });
+                            alert.show();
+                        });
 
-                pick.setOnClickListener(v -> {
-                    Intent intent = new Intent(Intent.ACTION_PICK);
-                    intent.setType("video/");
-                    startActivityForResult(intent,355);
-                });
-
-                play.setOnClickListener(v -> {
-                    if(mediaPlayer.isPlaying()){
-                        mediaPlayer.pause();
-                        play.setText("play");
-                    }
-                    else{
-                        mediaPlayer.start();
-                        play.setText("pause");
-                    }
-                });
-
-
-                AudioExtractor audioExtractor = new AudioExtractor();
-                convert.setOnClickListener(v -> {
-
-                    try {
-                        audioExtractor.VideotoAudio(sourcePath,destPath,true,false);
-                    } catch (IOException e) {
-                        e.printStackTrace();
                     }
 
-                    AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
-                    alert.setCancelable(true);
-                    alert.setCancelable(false);
-                    alert.setTitle("Convert Finished");
-                    alert.setMessage("Your file is saved in: "+destPath);
-                    alert.setPositiveButton("close", (dialog, which) -> {
-                        dialog.dismiss();
-                    });alert.show();
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
 
-                });
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+
+                        if (response.isPermanentlyDenied() == true) {
+                            AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                            alert.setCancelable(false);
+                            alert.setTitle("Permission");
+                            alert.setMessage("Give Permission to app: go to setting>permissions>storage and enable");
+                            alert.setPositiveButton("Go", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                    Uri uri = Uri.fromParts("package", getPackageName(), null);
+                                    intent.setData(uri);
+                                    startActivity(intent);
+                                }
+                            });
+                            alert.show();
+//                            Intent i = new Intent(MainActivity.this, MainActivity.class);
+//                            startActivity(i);
+                        }
+
+                        if (response.isPermanentlyDenied() == true) {
+                            convert.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    AlertDialog.Builder alert2 = new AlertDialog.Builder(MainActivity.this);
+                                    alert2.setCancelable(true);
+                                    alert2.setCancelable(false);
+                                    alert2.setTitle("Permission is not Given");
+                                    alert2.setMessage("Restart the app and give Permisson: ");
+                                    alert2.setPositiveButton("Restart", (dialog, which) -> {
+                                        Intent i = new Intent(MainActivity.this, MainActivity.class);
+                                        startActivity(i);
+                                    });
+                                    alert2.show();
+                                }
+                            });
+                        }
+                    }
+
+                }).check();
+
+
+
+        pick.setOnClickListener(v -> {
+
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType("video/");
+            startActivityForResult(intent,355);
+        });
+
+        play.setOnClickListener(v -> {
+            if(mediaPlayer.isPlaying()){
+                mediaPlayer.pause();
+                play.setText("play");
             }
-
-            @Override
-            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
-                permissionToken.continuePermissionRequest();
-
+            else{
+                mediaPlayer.start();
+                play.setText("pause");
             }
-        }).check();
+        });
+
+
+
     }
 
     @Override
@@ -134,9 +185,7 @@ public class MainActivity extends AppCompatActivity {
                      destPath = destDirectory+fileName;
                 }
 
-                surfaceView = findViewById(R.id.surfaceView);
-                play = findViewById(R.id.play);
-                convert = findViewById(R.id.convert);
+
 
                 play.setVisibility(View.VISIBLE);
                 convert.setVisibility(View.VISIBLE);
